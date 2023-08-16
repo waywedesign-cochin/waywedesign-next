f81a12164a11af2fa8cd455fb806a21a383a6992
libin v Baby <82673070+libinvbaby@users.noreply.github.com>
2023-08-16 16:36:18 +0530

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,11 +6,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
        
-      <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <Script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-537W3XM');`}}></script>
+})(window,document,'script','dataLayer','GTM-537W3XM');`}}></Script>
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
           rel="stylesheet"
@@ -60,8 +60,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       />
       <Script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" />
       <body>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-537W3XM"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      
         <Main />
         <NextScript />
       </body>
