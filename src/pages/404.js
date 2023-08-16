11226c8bb9f1ad378ff3437372fb89a35a852c41
libin v Baby <82673070+libinvbaby@users.noreply.github.com>
2023-08-16 16:57:55 +0530

    upated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,21 @@
+import Footer from "@/Components/Footer";
+import Menu from "@/Components/Navbar";
+import Link from 'next/link'
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+    return( 
+    <>
+    <Menu/>
+
+    <div className="container text-center">
+        <div>
+        <img width="50%" src="/assets/images/404.png" alt="" />
+
+        </div>
+        <Link style={{marginBottom:"10px",position:'relative',bottom:'80px'}} href="/" className="about-one__btn thm-btn">
+                  Back to Home
+                </Link>
+    </div>
+    <Footer/>
+    </>)
   }
