8d448f45c4d6f7c0d3114731a3c24deb3d5e1692
libin v Baby <82673070+libinvbaby@users.noreply.github.com>
2023-08-19 15:44:06 +0530

    updated

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-       
+      <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-7007M491Z3"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-7007M491Z3');
+              `,
+            }}
+          ></script>
       <Script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
