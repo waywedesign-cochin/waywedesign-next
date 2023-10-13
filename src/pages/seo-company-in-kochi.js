8c2a711b316f34a45823805f20e3673bc6277cda
libin v Baby <82673070+libinvbaby@users.noreply.github.com>
2023-10-13 13:23:01 +0530

    updated

--- src/pages/seo-company-in-kochi.js
+++ src/pages/seo-company-in-kochi.js
@@ -1,50 +1,202 @@
-import React from 'react'
+import Footer from "@/Components/Footer";
+import MobileMenu from "@/Components/MobileMenu";
+import Menu from "@/Components/Navbar";
+import React from "react";
+import Head from "next/head";
+import { motion } from "framer-motion";
+import webillustration from "../images/seo-company.png";
+import webicon from "../images/app-development-icon.png";
+import serviceSeo from "../images/seo-service.png";
+import design from "../images/design.png";
+import support from "../images/support.png";
+import cms from "../images/cms.png";
+import seo from "../images/seo.png";
+import Image from "next/image";
 
-const seoCompanyInKochi = () => {
+const SeoCompanyKochi = () => {
   return (
     <>
-            {/* <!--====== Hero Section Start ======--> */}
-       <section className="hero-section-three rel z-2 pt-235 rpt-150 pb-130 rpb-100">
-  <div className="container">
-    <div className="row align-items-center">
-      <div className="col-lg-6 col-md-11">
-        <div className="hero-content-three rpt-15 rmb-75">
-          <h1 className="mb-15 wow fadeInUp delay-0-2s">We’re Advanced analytics software solutions</h1>
-          <p className="wow fadeInUp delay-0-4s">Sit amet consectetur adipiscing elit seddo eiusmod tempode dunt ut labore et dolore magna aliqua suspendisse</p>
-          <form className="newsletter-form mt-40" action="#">
-            <div className="newsletter-email wow fadeInUp delay-0-6s">
-              <input type="email" placeholder="Enter Email Address" required />
-              <button type="submit">Get Free Trial <i className="fas fa-arrow-right" /></button>
-            </div>
-            <div className="newsletter-radios wow fadeInUp delay-0-8s">
-              <div className="custom-control custom-radio">
-                <input type="radio" className="custom-control-input" id="hero-wekly" name="example1" defaultChecked />
-                <label className="custom-control-label" htmlFor="hero-wekly">Wekly Updates</label>
-              </div> 
-              <div className="custom-control custom-radio">
-                <input type="radio" className="custom-control-input" id="hero-monthly" name="example1" />
-                <label className="custom-control-label" htmlFor="hero-monthly">Monthly Updates</label>
-              </div> 
-            </div>
-          </form>
-        </div>
-      </div>
-      <div className="col-lg-6">
-        <div className="hero-image-three overlay wow fadeInLeft delay-0-4s">
-          <img src="assets/images/hero/hero-three.jpg" alt="Hero" />
-        </div>
-      </div>
-    </div>
-  </div>
-  <img className="dots-shape" src="assets/images/shapes/dots.png" alt="Shape" />
-  <img className="tringle-shape" src="assets/images/shapes/tringle.png" alt="Shape" />
-  <img className="close-shape" src="assets/images/shapes/close.png" alt="Shape" />
-</section>
+      <motion.div
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: 100 }}
+        transition={{ duration: 1 }}
+      >
+        <Head>
+          <Head>
+            <title>
+              Web Designing Company in Cochin | Web Development Company in Kochi{" "}
+            </title>
 
-        {/* <!--====== Hero Section End ======--></img> */}
-    
-    </>
-  )
+            <meta
+              name="description"
+              content="We are award-wining web design & development company in Kochi, Kerala. We specialise in web design, and SEO-friendly websites with 100% satisfaction."
+            />
+            <meta name="robots" content="index, follow" />
+            <meta
+              name="googlebot"
+              content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+            />
+            <meta
+              name="bingbot"
+              content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+            />
+            <link
+              rel="canonical"
+              href="https://waywedesign.com/web-development-company-in-kochi"
+            />
+            <meta property="og:locale" content="en_US" />
+            <meta property="og:type" content="website" />
+            <meta
+              property="og:title"
+              content="Best Web Design and Development Company Kochi"
+            />
+            <meta
+              property="og:description"
+              content="Visit the Best Web Develpoment company in kochi and Web Design company in kochi"
+            />
+            <meta
+              property="og:url"
+              content="https://waywedesign.com/web-development-company-in-kochi"
+            />
+            <meta property="og:image" content="" />
+            <meta property="og:image:width" content="1024" />
+            <meta property="og:image:height" content="1024" />
+            <meta
+              property="og:image:alt"
+              content="Web Design and Development  Company in Kochi"
+            />
+            <meta property="og:image:type" content="image/png" />
+            <meta property="og:site_name" content="WayWeDesign" />
+            <meta
+              property="article:publisher"
+              content="https://www.facebook.com/waywedesign"
+            />
+            <meta
+              property="article:modified_time"
+              content="2023-06-05T08:16:17+00:00"
+            />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:creator" content="@waywedesign" />
+            <meta name="twitter:site" content="@waywedesign" />
+
+            <script type="application/ld+json">
+{
+`{
+  "@context": "http://schema.org", 
+  "@type": "WebPage",
+   "name": "Best Web Design and Development Company Kochi", 
+   "description": "Visit the Best Web Develpoment company in kochi and Web Design company in kochi",
+    "url": "https://waywedesign.com/web-development-company-in-kochi"
+}`
 }
 
-export default seoCompanyInKochi
+</script>
+ 
+          </Head>
+        </Head>
+        <Menu />
+        <MobileMenu />
+        <div className="seoCompanybgImage"></div>
+        <section className="SeoCompanyKochi">
+          <div className="container py-4">
+            <div className="row">
+              <div className="col-md-6 align-self-center ">
+                <h1 className="text-light  webDevelopmentCompanyBannerTitle">
+                BEST SEO COMPANY IN KOCHI
+                </h1>
+                <p className="text-light">
+                The best SEO Company that helps to rank your websites and brings you organic traffic and leads
+
+                </p>
+                <button className="btn btn-danger btn-lg">Learn More</button>
+              </div>
+              <div className="col-md-6 align-self-center">
+                <Image
+                  className="bannerImagewebDevelopmentCompany"
+                  src={webillustration}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="pt-5">
+          <div className="container">
+            {/* <h3 className="py-2 text-center">WEB DESIGN & DEVELOPMENT</h3> */}
+            <p>
+            If you came to this SEO page, you are exploring the best SEO service in Kochi. At Waywedesign we do what we said and understand the latest techniques for ranking on Google. We can offer you highly modified SEO strategies articulated through an in-depth and broad study of multiple factors. Search Engine Optimization (SEO) is the lifeblood of any business in the Internet era. Waywedesign is at the pole position offering entities with superior visibility on Google. Our services have delivered remarkable advancements in ROI for businesses World Wide. A noticeable and active online presence on the search engines is the key to acquiring potential customers and improving the brand name, and that is what we promise you. With our custom month-to-month approach to SEO, Google will rank you #1 and we offer reliable service for your business.  Contact us today for a free SEO consultation with us.
+
+            </p>
+            <p>
+              The substantial industry experience, the team of web development
+              experts, and the customer-centric approach make us the No.1 web
+              designing company in Kochi. In our web development company, we
+              follow a series of strategic schemes that refine your objectives
+              and target into a methodic, feasible development plan. We deliver
+              the websites according to our client's requirements and we build
+              them with a suitable programming language or from theme-based
+              websites like WordPress, Wix, etc.
+            </p>
+          </div>
+        </section>
+        
+        {/* designing and redesigning  start */}
+        <section className="py-4">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <h2 className="text-center py-3 fs-3">BEST SEO COMPANY IN KOCHI THAT PROVIDE THE BEST SEO SERVICES</h2>
+                <p className="text-justify py-2">
+                A customer would purchase a product /service only after researching online, imagine your product or service is not on the top list of Google, Will your product gets sold easily? No right. They only click the website which is listed on top. We help your website ranking top positions in the natural search results. And we only use ethical and white hat SEO methods to secure top positions in search rankings, ensuring that the results are more effective and everlasting. Our SEO company is breaking far away from restrictive service plans and developing absolutely custom digital marketing methods that take you to the front page.
+                </p>
+              </div>
+              <div className="col-md-12">
+                <h3 className="text-center py-3">Haven't you started yet?
+</h3>
+                <p className="text-justify py-2">
+                People are searching for particular products and services, you may be a supplier of those products or services, but never got a call for an order. Then it's time to think of a remedy. Our SEO Company provides the most effective methods for ranking your website on the top list of Google. SEO has become the trendy acronym amongst aspiring entrepreneurs. It is jargon that is often used to convince someone that they are skilled in their own business. We at Waywedesign proudly providing quality SEO for years. As an authority in SEO, we take chance to educate the entrepreneurs. We use it as an opportunity to show the light!
+                </p>
+              </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6">
+                    <h3 className="py-2">SKYROCKET YOUR PAGERANK</h3>
+                    <p>
+                    Now You are looking for SEO as you clearly know that if you want to maximize your ROI then your website has to be on the top page of Google. The search engine is constantly updating algorithms and that is the main reason most SEO companies are failing. So that the majority of small, mid, large-sized companies hire Waywedesign for performing SEO.
+
+<br/>
+We give a new life to your website, updating just once a month doesn’t help your website to rank better. Choose Waywedesign to handle your SEO as we have got experienced over the years and we have been learning SEO day-day and we provide the best SEO service to our clients and we change the rules as google does.
+
+                    </p>
+                </div>
+                <div className="col-md-6 align-self-center text-center">
+                    <Image src={serviceSeo} alt="seo services" />
+                </div>
+            </div>
+          </div>
+        </section>
+        {/* designing and redesigning  end */}
+        <section>
+            <div className="container">
+                <div className="row">
+                    <h3>Proven SEO Strategy
+</h3>
+<p>We can find your website’s current status and we are able to make it on top of Google. We implement our strategy and continue to make changes and develop it to ensure that your goals are met so that you can get maximum ROI. We can create an in-depth analysis which is significant for any SEO. We understand that SEO is the main arena for competition in sales especially in the modern-day so we place the utmost importance on a clean and ethical white hat SEO strategy that will increase traffic and quality leads for your business. No outsourcing or no external work. We are waiting for you.</p>
+               
+<h3>Hire Waywedesign for your SEO Service Partner
+
+</h3>
+             <p>Our SEO services help you create a strong online presence that makes way for customers to your website. Waywedesign is led by an experienced managing team of Digital Marketing veterans that founded the firm on the principle of providing honest and amazing search engine optimization service.  We guarantee that our SEO strategies will deliver real results for our clients. For all your online marketing solutions hire us!
+</p>  
+                </div>
+            </div>
+
+        </section>
+
+        <Footer />
+      </motion.div>
+    </>
+  );
+};
+
+export default SeoCompanyKochi;
