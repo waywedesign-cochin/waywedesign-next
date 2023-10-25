621b55a65c0326b11940f96bb2c597c3fc8085a3
libin v Baby <82673070+libinvbaby@users.noreply.github.com>
2023-10-26 00:52:02 +0530

    updated container for privacy policy page

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -41,6 +41,7 @@ const PrivacyPolicy =()=>{
           </Head>
         <Menu />
         <MobileMenu />
+        <div className="container">
         <p className="pt-4">Way WeDesign is entrusted to safeguarding your privacy. By entering our site, you agree to the terms of our privacy policy to collect, use, and disclose your personal data when you use our Service.</p>
         <p>
 Your data will be used to study and improve our services methods. By using the Service, you agree to collect and use of information in accordance with this policy. We collect following types of information such as your name, email address, telephone number, address, cookies and data usage for better service interpretation and that information will be kept private and confidential.
@@ -48,7 +49,8 @@ Your data will be used to study and improve our services methods. By using the S
 <p><b>Use of Data:</b> We collect information on how the Service were accessed and used. Usage of Data may include information such as your computer's Internet IP, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on our pages, unique device identifiers and other diagnostic data.</p>
 <p><b>Cookies & Tracking: </b>We use cookies and similar tracking technologies to track your activity and hold certain information. Cookies are sent to your browser and stored on your device. We use them for various purposes, provide and maintain the Service, notify about changes in our Service, allow you to participate in interactive features of our Service when you choose to do so, provide customer care and support etc..</p>
 <p className="pb-4"><b>Security:</b>All necessary steps are taken to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or any countries without our consent. Your data is important to us and is 100% secure.  </p>
-            <Footer/>
+</div>
+<Footer/>
         </>
     )
 }
