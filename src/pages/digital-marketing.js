3c993ba62b03c92d3ba57761e330825558f0ac77
libin v Baby <82673070+libinvbaby@users.noreply.github.com>
2023-11-01 23:37:13 +0530

    updated link on digital marketing page

--- src/pages/digital-marketing.js
+++ src/pages/digital-marketing.js
@@ -91,8 +91,8 @@ const DigitalMarketing = () => {
                     and products more efficiently. </p>
                     <p className="service-details__text ">
 
-                    Way Wedesign is a dynamic, versatile, and full-service
-                    digital marketing company that is always in pursuit to offer
+                    Way Wedesign is a dynamic, versatile, and full-service <Link  to="/blog/the-history-and-evolution-of-digital-marketing"> digital marketing</Link>
+                    company that is always in pursuit to offer
                     reliable and result-oriented digital marketing services. We
                     trust our own <Link href="/search-engine-optimization">search engine optimization (SEO)</Link>  and marketing
                     strategies to drive new customers to your website. With
