ed5da7d4ab012ae7dbf23c113e8858ce14207ed0
wwd <waywedesign@gmail.com>
2023-12-10 20:16:28 +0530

    remove h1 tag with span

--- src/Components/Home/PartnerSlider.js
+++ src/Components/Home/PartnerSlider.js
@@ -3,48 +3,64 @@ import CountUp from 'react-countup'
 import ScrollTrigger from 'react-scroll-trigger'
 
 const PartnerSlider = () => {
-  const [counterOn, setCounterOn] = useState(false)
+  const [ counterOn, setCounterOn ] = useState( false )
   return (
     <div>
-      {/*Brand One Start*/}
+      {/*Brand One Start*/ }
       <section className="brand-one">
-        <ScrollTrigger onEnter={()=>setCounterOn(true)} onExit={()=>setCounterOn(false)}>
+        <ScrollTrigger onEnter={ () => setCounterOn( true ) } onExit={ () => setCounterOn( false ) }>
           <div className="container">
-            
+
             <div className="row">
               <div className="col-md-3 col-6 text-center ">
                 <div className="counter-wrapper">
-                  <h1 className=" section-title__title_counter text-white">
-                    {counterOn && <CountUp start={0} end={100} duration={2} delay={0}/>}
+                  {/* <h1 className=" section-title__title_counter text-white">
+                    { counterOn && <CountUp start={ 0 } end={ 100 } duration={ 2 } delay={ 0 } /> }
                     +
-                  </h1>
+                  </h1> */}
+                  <span className="section-title__title_counter text-white">
+                    { counterOn && <CountUp start={ 0 } end={ 100 } duration={ 2 } delay={ 0 } /> }
+                    <span style={ { fontSize: '50px', fontWeight: '600' } }>+</span>
+                  </span>
                   <h4 className="text-white">Projects</h4>
                 </div>
               </div>
               <div className="col-md-3 col-6 text-center counter-col">
                 <div className="counter-wrapper">
-                  <h1 className=" section-title__title_counter text-white">
-                    {counterOn && <CountUp start={0} end={10} duration={2} delay={0}/>}
+                  {/* <h1 className=" section-title__title_counter text-white">
+                    { counterOn && <CountUp start={ 0 } end={ 10 } duration={ 2 } delay={ 0 } /> }
                     +
-                  </h1>
+                  </h1> */}
+                  <span className="section-title__title_counter text-white">
+                    { counterOn && <CountUp start={ 0 } end={ 10 } duration={ 2 } delay={ 0 } /> }
+                    <span style={ { fontSize: '50px', fontWeight: '600' } }>+</span>
+                  </span>
                   <h4 className="text-white">Countries</h4>
                 </div>
               </div>
               <div className="col-md-3 py-md-0 py-3 col-6 text-center counter-col">
                 <div className="counter-wrapper">
-                  <h1 className=" section-title__title_counter text-white">
-                    {counterOn && <CountUp start={0} end={10} duration={2} delay={0}/>}
+                  {/* <h1 className=" section-title__title_counter text-white">
+                    { counterOn && <CountUp start={ 0 } end={ 10 } duration={ 2 } delay={ 0 } /> }
                     +
-                  </h1>
+                  </h1> */}
+                  <span className="section-title__title_counter text-white">
+                    { counterOn && <CountUp start={ 0 } end={ 100 } duration={ 2 } delay={ 0 } /> }
+                    <span style={ { fontSize: '50px', fontWeight: '600' } }>+</span>
+                  </span>
                   <h4 className="text-white">Services</h4>
                 </div>
               </div>
               <div className="col-md-3 py-md-0 py-3  col-6 text-center counter-col">
                 <div className="counter-wrapper">
-                  <h1 className=" section-title__title_counter text-white">
-                    {counterOn && <CountUp start={0} end={100} duration={2} delay={0}/>}
+                  {/* <h1 className=" section-title__title_counter text-white">
+                    { counterOn && <CountUp start={ 0 } end={ 100 } duration={ 2 } delay={ 0 } /> }
                     %
-                  </h1>
+                  </h1> */}
+                  <span className="section-title__title_counter text-white">
+                    { counterOn && <CountUp start={ 0 } end={ 100 } duration={ 2 } delay={ 0 } /> }
+                    <span style={ { fontSize: '50px', fontWeight: '600' } }>%</span>
+                  </span>
                   <h4 className="text-white">Reliable</h4>
                 </div>
               </div>
@@ -52,7 +68,7 @@ const PartnerSlider = () => {
           </div>
         </ScrollTrigger>
       </section>
-      {/*Brand One End*/}
+      {/*Brand One End*/ }
     </div>
   )
 }
