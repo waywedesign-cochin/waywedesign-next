0f3f00167cba8f05d614e6cf396cc3382e03e0e0
Jithun <waywedesign@gmail.com>
2023-12-12 17:49:32 +0530

    x icon changed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,6 +5,7 @@ import logo from '../images/logo.png'
 import whatsapp from "../images/whatsapp.png";
 import elioplusBadge from "../images/elioplus_badge.png";
 
+
 const Footer = () => {
   return (
     <div>
@@ -55,7 +56,10 @@ const Footer = () => {
                           <i className="fab fa-behance" />
                         </Link>
                         <Link target={"_blank"} href="https://twitter.com/waywedesign">
-                          <i className="fab fa-twitter" />
+                          {/* <i className="fab fa-twitter" />                        */}
+                          <div onmouseover="changeColor('black')" >
+                          <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"  fill='white' className='x-twitter' onmouseover='black'><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+                          </div>
                         </Link>
                         <Link target={"_blank"} href="https://www.facebook.com/contact.waywedesign/">
                           <i className="fab fa-facebook" />
@@ -168,7 +172,7 @@ const Footer = () => {
                           </div>
                           <div className="text">
                             <p>
-                              <a className="fw-light" href="tel:+917994645676">+91 7994645676</a>
+                              <a className="fw-light" href="tel:+917994645676">+91 799 464 5676</a>
                             </p>
                           </div>
                         </li>
@@ -192,7 +196,7 @@ const Footer = () => {
                           </div>
                           <div className="text">
                             <p>
-                              <a className="fw-light" href="tel:+917994643780">+91 7994643780</a>
+                              <a className="fw-light" href="tel:+917994643780">+91 799 464 3780</a>
                             </p>
                           </div>
                         </li>
