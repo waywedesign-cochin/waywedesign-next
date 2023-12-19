9fa6428a7c17c6bc094f453fd828b562c1e81bf3
Jithun <waywedesign@gmail.com>
2023-12-19 15:02:29 +0530

    twitter icon change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,6 +4,7 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 // import logo from '../../images/logo-wwd.png'
 import  Link  from 'next/link';
+import { color } from "framer-motion";
 const Menu = () => {
   return (
     <div className="full_width_nav">
@@ -64,7 +65,8 @@ const Menu = () => {
                   <i className="fab fa-behance" />
                 </Link>
                 <Link target={"_blank"} href="https://twitter.com/waywedesign">
-                  <i className="fab fa-twitter" />
+                  {/* <i className="fab fa-twitter" /> */}
+                          <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"  fill='white' className='fab fa-twitter' ><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
                 </Link>
                 <Link
                   target={"_blank"}
